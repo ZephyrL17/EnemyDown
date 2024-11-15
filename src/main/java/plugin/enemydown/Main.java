f0bb80525172c625b218b4fa0a934ef11eaa4be5
ZephyrL17 <jackknife0107@gmail.com>
2024-11-15 16:08:43 +0900

    DAY17

--- src/main/java/plugin/enemydown/Main.java
+++ src/main/java/plugin/enemydown/Main.java
@@ -1,19 +1,14 @@
 package plugin.enemydown;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Enemy;
-import org.bukkit.entity.EntityType;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.enemydown.command.EnemyDownCommand;
 
-public final class main extends JavaPlugin{
+public final class Main extends JavaPlugin{
 
   @Override
   public void onEnable() {
-    EnemyDownCommand enemyDownCommand = new EnemyDownCommand();
+    EnemyDownCommand enemyDownCommand = new EnemyDownCommand(this);
     Bukkit.getPluginManager().registerEvents(enemyDownCommand, this);
     getCommand("enemyDown").setExecutor(enemyDownCommand);
   }
